744dd276048a05c55cdcb25fa7279f74956df9f6
Gorm-Erik Aarsheim <gormerik96@gmail.com>
2019-10-06 15:17:03 +0200

    Changed primary colors

--- brukerfeil-presentation/src/GroupPresentation/index.tsx
+++ brukerfeil-presentation/src/GroupPresentation/index.tsx
@@ -4,7 +4,16 @@ import styles from "./styles.module.css";
 const GroupPresentation = () => {
 	return (
 		<div className={styles.container}>
-			<h1>Gruppe</h1>
+			<h2>Gruppe</h2>
+			<div>
+				<p>
+					Brukerfeil er en gruppe på seks gutter, som har jobber sammen i over
+					to år. Vi er en engasjert og sammensveiset gjeng, som alltid er klare
+					<br></br>
+					for nye utfordringer. Gruppa ser etter en oppdragsgiver som kan by på
+					et teknisk prosjekt, samt oppfølging.
+				</p>
+			</div>
 		</div>
 	);
 };
