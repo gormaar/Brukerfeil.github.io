50550e5e054dd5c459a576cac706542df82d306b
Gorm-Erik Aarsheim <gormerik96@gmail.com>
2019-10-06 14:35:04 +0200

    Post migration changes

---
+++ brukerfeil-presentation/src/Contact/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+const Contact = () => {
+	return (
+		<div className={styles.container}>
+			<h3>Contact form</h3>
+		</div>
+	);
+};
+
+export default Contact;
---
+++ brukerfeil-presentation/src/MemberPresentation/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+const MemberPresentation = () => {
+	return (
+		<div className={styles.container}>
+			<h1>Om oss</h1>
+		</div>
+	);
+};
+
+export default MemberPresentation;
